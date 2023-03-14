2ab78373939dba13e1dcab8170a5208f77a61476
jeffry zaman <jeffry.zaman@gmail.com>
2023-03-14 17:13:26 -0400

    initial commit - initial setup

---
+++ src/main/kotlin/com/example/devopshubintergrationservice/DevopsHubIntegrationServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.devopshubintergrationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevopsHubIntegrationServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevopsHubIntegrationServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/devopshubintergrationservice/DevopsHubIntegrationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.devopshubintergrationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevopsHubIntegrationServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
